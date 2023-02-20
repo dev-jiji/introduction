b8f1c1a042a8963fc7394ff0057df6c1b98087b9
jieun <wldmsdlsns1@gmail.com>
2023-02-20 13:20:14 +0900

    기본 개인프로필 구성잡기,portfolio 파일까지 함.

---
+++ src/pages/Visual.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Visual = () => {
+    return (
+        <div className="visual">
+            <div className="inner">
+                <img src="/images/visual.png" alt="visual" />
+            </div>
+        </div>
+    );
+};
+
+export default Visual;
