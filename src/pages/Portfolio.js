dffc882cb3da22edcbb37e5e0d8c9acc63fbbe10
jieun <wldmsdlsns1@gmail.com>
2023-03-13 14:26:14 +0900

    Merge branch 'main' of https://github.com/dev-jiji/introduction
    # Conflicts:
    #	package.json
    #	src/pages/Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,25 +2,26 @@ import React from "react";
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
-import "swiper/css/navigation";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Portfolio = () => {
     const path = process.env.PUBLIC_URL;
+
     return (
         <section className="portfolio scroll">
             <div className="inner">
-                {/* 개발담당자 : 팀프로젝트, 개인 작업(5개 목표(퍼블리싱 > 프론트)) */}
+                {/* 개발 담당자 : 팀프로젝트, 개인 작업(5개 목표(퍼블리싱 -> 프론트)) */}
                 <h2 data-aos="fade-up">Portfolio</h2>
                 <div className="contents">
-                    {/* 팀프로젝트 */}
+                    {/* 팀 프로젝트 */}
                     <div className="portfolio-box">
-                        <h3 data-aos="fade-up">Team Project</h3>
+                        <h3>Team Project</h3>
                         <div className="portfolio-team">
                             <div className="portfolio-left">
                                 <a
-                                    href="https://dev-jiji.github.io/hansalim/"
+                                    href="https://devparkse.github.io/hansalim/"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     alt="portfolio"
@@ -33,7 +34,7 @@ const Portfolio = () => {
                                 (이미지 클릭시 프로젝트 페이지로 이동합니다.)
                             </div>
                             <div className="portfolio-right">
-                                <ul className="portfilio-team-list">
+                                <ul className="portfolio-team-list">
                                     <li>프로젝트명 : </li>
                                     <li>프로젝트 기간 : </li>
                                     <li>프로젝트 주제 : </li>
@@ -42,9 +43,9 @@ const Portfolio = () => {
                                     <li>프로젝트 사용언어 : </li>
                                     <li>프로젝트 피그마 : </li>
                                     <li>
-                                        프로젝트 Git :{" "}
+                                        프로젝트 Git :
                                         <a
-                                            href="https://github.com/dev-jiji/hansalim"
+                                            href="https://github.com/devparkse/hansalim"
                                             target="_blank"
                                             rel="noopener noreferrer"
                                             alt="portfolio"
@@ -72,7 +73,7 @@ const Portfolio = () => {
                                 (이미지 클릭시 프로젝트 페이지로 이동합니다.)
                             </div>
                             <div className="portfolio-right">
-                                <ul className="portfilio-team-list">
+                                <ul className="portfolio-team-list">
                                     <li>프로젝트명 : </li>
                                     <li>프로젝트 기간 : </li>
                                     <li>프로젝트 주제 : </li>
@@ -101,7 +102,7 @@ const Portfolio = () => {
                                 (이미지 클릭시 프로젝트 페이지로 이동합니다.)
                             </div>
                             <div className="portfolio-right">
-                                <ul className="portfilio-team-list">
+                                <ul className="portfolio-team-list">
                                     <li>프로젝트명 : </li>
                                     <li>프로젝트 기간 : </li>
                                     <li>프로젝트 주제 : </li>
@@ -130,7 +131,7 @@ const Portfolio = () => {
                                 (이미지 클릭시 프로젝트 페이지로 이동합니다.)
                             </div>
                             <div className="portfolio-right">
-                                <ul className="portfilio-team-list">
+                                <ul className="portfolio-team-list">
                                     <li>프로젝트명 : </li>
                                     <li>프로젝트 기간 : </li>
                                     <li>프로젝트 주제 : </li>
@@ -253,178 +254,8 @@ const Portfolio = () => {
                     </div>
                 </div>
             </div>
-            <div className="portfolio-team">
-              <div className="portfolio-left">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="project" />
-                </a>
-                (이미지 클릭시 프로젝트 페이지로 이동합니다.)
-              </div>
-              <div className="portfolio-right">
-                <ul className="portfilio-team-list">
-                  <li>프로젝트명 : </li>
-                  <li>프로젝트 기간 : </li>
-                  <li>프로젝트 주제 : </li>
-                  <li>프로젝트 기여도 : </li>
-                  <li>프로젝트 도구 : </li>
-                  <li>프로젝트 사용언어 : </li>
-                  <li>프로젝트 피그마 : </li>
-                  <li>프로젝트 Git : </li>
-                  <li>프로젝트 노션 : </li>
-                </ul>
-              </div>
-            </div>
-            <div className="portfolio-team">
-              <div className="portfolio-left">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="project" />
-                </a>
-                (이미지 클릭시 프로젝트 페이지로 이동합니다.)
-              </div>
-              <div className="portfolio-right">
-                <ul className="portfilio-team-list">
-                  <li>프로젝트명 : </li>
-                  <li>프로젝트 기간 : </li>
-                  <li>프로젝트 주제 : </li>
-                  <li>프로젝트 기여도 : </li>
-                  <li>프로젝트 도구 : </li>
-                  <li>프로젝트 사용언어 : </li>
-                  <li>프로젝트 피그마 : </li>
-                  <li>프로젝트 Git : </li>
-                  <li>프로젝트 노션 : </li>
-                </ul>
-              </div>
-            </div>
-            <div className="portfolio-team">
-              <div className="portfolio-left">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="project" />
-                </a>
-                (이미지 클릭시 프로젝트 페이지로 이동합니다.)
-              </div>
-              <div className="portfolio-right">
-                <ul className="portfilio-team-list">
-                  <li>프로젝트명 : </li>
-                  <li>프로젝트 기간 : </li>
-                  <li>프로젝트 주제 : </li>
-                  <li>프로젝트 기여도 : </li>
-                  <li>프로젝트 도구 : </li>
-                  <li>프로젝트 사용언어 : </li>
-                  <li>프로젝트 피그마 : </li>
-                  <li>프로젝트 Git : </li>
-                  <li>프로젝트 노션 : </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          {/* 개인 스터디 프로젝트 */}
-          <div className="portfolio-box">
-            <h3>Study Project</h3>
-            {/* Swiper 배치 */}
-            <Swiper
-              loop={true}
-              slidesPerView={1}
-              spaceBetween={0}
-              pagination={true}
-              navigation={true}
-              breakpoints={{
-                860: {
-                  slidesPerView: 1,
-                  spaceBetween: 0,
-                },
-                960: {
-                  slidesPerView: 2,
-                  spaceBetween: 30,
-                },
-                1180: {
-                  slidesPerView: 3,
-                  spaceBetween: 30,
-                },
-              }}
-              modules={[Pagination, Navigation]}
-              className="portfolio-slide"
-            >
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-              <SwiperSlide>
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  alt="portfolio"
-                >
-                  <img src="/images/sample.png" alt="portfolio" />
-                </a>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Portfolio;
